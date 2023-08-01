823ba37f4d7a48bc6d33ec9f4cf2689500c4543d
AMANI Eric <amaniericus@gmail.com>
2023-08-01 22:45:01 +0200

    ch(setup): eslint and prettier

---
+++ src/graphql/graphql.module.ts
@@ -0,0 +1,12 @@
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { Module } from "@nestjs/common";
+import { GraphQLModule } from "@nestjs/graphql";
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver
+    })
+  ]
+})
+export class AppModule {}
